21f750d8fdd5050bfc82b3a3ffdee3510afabfe7
Alexander <alexander.pp8@gmail.com>
2025-03-28 12:27:03 +0100

    ASP-SEC 4 Gemaakt

---
+++ ASP-SEC/AspSec4/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppSec4.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
